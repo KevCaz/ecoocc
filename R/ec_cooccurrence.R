e0f4f55212f8840e5f7228bd4ae747c156f3e116
Kevin Cazelles <kcazelle@uoguelph.ca>
2018-03-07 18:01:54 -0500

    edit readme :book:

--- R/ec_cooccurrence.R
+++ R/ec_cooccurrence.R
@@ -17,27 +17,27 @@
 #' A matrix with species combination and co-occurrence values associated.
 #'
 #' @examples
-#' mat <- matrix(stats::runif(60000)>.2,  ncol = 6)
+#' mat <- matrix(stats::runif(60000)>.8,  ncol = 6)
 #' out <- ec_cooccurrence(mat, test = c('ra', 've'))
-#' plot(out$zs_raw, out$zs_veech)
+#' plot(out$zs_raw*sqrt(0.8), out$zs_veech)
+#' abline(0,1)
 
 ec_cooccurrence <- function(mat_pa, species_names = NULL, test = NULL) {
     mat <- as.matrix(mat_pa) > 0
     stopifnot(ncol(mat) > 1)
     out <- cooccurrence_core(mat)
-    #
+    # 
+    if ("ra" %in% test) 
+        out$zs_raw <- test_cooccurrence_raw_core(out$case_sp1, out$case_sp2, out$case_11, 
+            nrow(mat))
+    if ("ve" %in% test) 
+        out$zs_veech <- test_cooccurrence_veech_core(out$case_sp1, out$case_sp2, 
+            out$case_11, nrow(mat))
+    # 
     if (!is.null(species_names)) {
         stopifnot(length(species_names) == ncol(mat))
         out[1L] <- species_names[out[, 1L]]
         out[2L] <- species_names[out[, 2L]]
     }
-    #
-    if ("ra" %in% test)
-        out$zs_raw <- test_cooccurrence_raw_core(out$case_sp1, out$case_sp2, out$case_11,
-            nrow(mat))
-    if ("ve" %in% test)
-        out$zs_veech <- test_cooccurrence_veech_core(out$case_sp1, out$case_sp2,
-            out$case_11, nrow(mat))
-    #
     out
 }
--- README.md
+++ README.md
@@ -5,6 +5,11 @@
 [![Build Status](https://travis-ci.org/KevCaz/ecoocc.svg?branch=master)](https://travis-ci.org/KevCaz/ecoocc)
 [![Build status](https://ci.appveyor.com/api/projects/status/qeywntin8yma0jb0?svg=true)](https://ci.appveyor.com/project/KevCaz/ecoocc)
 
+## Installation
+
+```R
+devtools::install.packages("KevCaz/ecoocc")
+```
 
 ## What is implemented so far?
 
