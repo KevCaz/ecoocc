b164b5f15e99318e787941ef28288621885b5070
Kevin Cazelles <kcazelle@uoguelph.ca>
2018-03-21 13:35:08 -0400

    :+1: unit testing for ec_betadiversity() :shield:

--- tests/testthat/test-betadiversity.R
+++ tests/testthat/test-betadiversity.R
@@ -4,12 +4,15 @@ mat <- matrix(c(0,0,1,1,1,0), ncol = 2)
 res1 <- ec_betadiversity(mat)
 res2 <- ec_betadiversity(mat, "ra")
 res3 <- ec_betadiversity(mat, c("bc", "wi"))
+#
 mat2 <- matrix(c(0,1,1,1,1,0), ncol = 2)
 res4 <- ec_betadiversity(mat2, c("bc", "wi"))
+res5 <- ec_betadiversity(mat2, site_names = LETTERS[1:3])
 
 test_that("expected errors", {
   expect_error(ec_betadiversity(c(1)), "nrow(mat) > 1 is not TRUE", fixed = TRUE)
   expect_error(ec_betadiversity(mat, c("bcz")), "any(methods %in% c(\"ra\", \"bc\", \"wi\")) is not TRUE", fixed = TRUE)
+  expect_error(ec_betadiversity(mat2, site_names = LETTERS[1:2]), "length(site_names) == nrow(mat_pa) is not TRUE", fixed = TRUE)
 })
 
 test_that("check data.frame names", {
@@ -17,6 +20,8 @@ test_that("check data.frame names", {
   expect_true(all(names(res2) == c("site1", "site2", "site1_only", "site2_only", "both", "none")))
   expect_true(all(names(res3) == c("site1", "site2", "bc", "wi")))
   expect_true(nrow(res1) == 3)
+  expect_true(all(res5$site1 == LETTERS[c(1, 1, 2)]))
+  expect_true(all(res5$site2 == LETTERS[c(2, 3, 3)]))
 })
 
 
